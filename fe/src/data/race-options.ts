03b6248fd7cc3205bf2833ed6ce13fdf380ee901
Mayonaise <genjinie7@gmail.com>
2026-05-04 16:31:40 +0900

    fix: 종족 설정값 jsonl파일 내용을 ts파일로 프론트 폴더에 추가

---
+++ fe/src/data/race-options.ts
@@ -0,0 +1,16 @@
+export interface RaceOption {
+  value: string;
+  label: string;
+}
+
+export const raceOptions: RaceOption[] = [
+  { value: "Gnome", label: "노움" },
+  { value: "Dragonborn", label: "드래곤본" },
+  { value: "Dwarf", label: "드워프" },
+  { value: "Elf", label: "엘프" },
+  { value: "Human", label: "인간" },
+  { value: "Tiefling", label: "티플링" },
+  { value: "Half-Elf", label: "하프엘프" },
+  { value: "Half-Orc", label: "하프오크" },
+  { value: "Halfling", label: "하플링" },
+];
